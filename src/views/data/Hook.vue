401613e2444ff70c74fa37d9cf5b08a8a00b8ed3
CaoMeiYouRen <996881204@qq.com>
2024-04-15 15:31:00 +0800

    fix: 修复 通知 和 webhook 的类型转换问题

--- src/views/data/Hook.vue
+++ src/views/data/Hook.vue
@@ -11,12 +11,12 @@
 <script lang="ts" setup>
 import { ref, watch } from 'vue'
 import { omit } from 'lodash-es'
-import { Hook, NotificationConfig } from '@/api/api'
-import { remove$key } from '@/utils/helper'
+import { Hook } from '@/api/api'
+import { objectValueToString, remove$key, stringValueToObject } from '@/utils/helper'
 const form = ref<Hook>({} as any)
 const postGet = (data: Hook) => {
-    if (data.type === 'notification' && typeof (data.config as NotificationConfig).config === 'object') {
-        (data.config as any).config = JSON.stringify((data.config as NotificationConfig).config, null, 4)
+    if (['notification', 'webhook'].includes(data.type)) {
+        (data.config as any) = objectValueToString(data.config as any)
     }
     return data
 }
@@ -25,9 +25,8 @@ const pre = (data: Hook) => {
     if ((data?.config as any)?._type ) {
         delete (data.config as any)._type
     }
-    if (data.type === 'notification' && typeof (data.config as NotificationConfig).config === 'string') {
-        (data.config as any).config = JSON.parse((data.config as any).config)
-
+    if (['notification', 'webhook'].includes(data.type)) {
+        (data.config as any) = stringValueToObject(data.config as any)
     }
     data.config = omit(data.config as any, ['0', '1']) as any
     data.config = remove$key(data.config as any) as any
